c5bd7c59cff40d4ebbb36ced6c90f21b30ff3295
visualstudio <visualstudio@122-PC16>
2019-01-30 11:02:10 -0500

    first change

---
+++ C0726863-week-04/C0726863-week-04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0726863_week_04
+{
+    class Program
+    {
+        class Village
+        
+        
+        // Node is an ADT
+         // what kind of DATA do we need in a NODE?
+        public Village nextVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+    }
+}
+        
+    
