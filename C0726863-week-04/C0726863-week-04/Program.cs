83aa367d35475e8757e4bf9c26e42703e75c86e0
visualstudio <visualstudio@122-PC16>
2019-01-30 11:25:02 -0500

    comit

--- C0726863-week-04/C0726863-week-04/Program.cs
+++ C0726863-week-04/C0726863-week-04/Program.cs
@@ -9,13 +9,31 @@ namespace C0726863_week_04
     class Program
     {
         class Village
-        
-        
-        // Node is an ADT
-         // what kind of DATA do we need in a NODE?
-        public Village nextVillage;
-        public string VillageName;
-        public bool isAstrildeHere = false;
+
+        {
+            // Node is an ADT
+            // what kind of DATA do we need in a NODE?
+            public Village nextVillage;
+            public string VillageName;
+            public bool isAstrildeHere = false;
+        }
+        class countryside
+        {
+            Village Maple = new Village();
+            Village Toronto = new Village();
+            Village Ajex = new Village();
+            Village First;
+            Village Last;
+
+
+            public void Launch()
+            {
+                Maple.VillageName = "Maple";
+                Maple.nextVillage = "Toronto";
+                Maple.nextVillage = "Toronto";
+            }
+
+        }
     }
 }
         
