eb9f872c779dcc0462468c9cae3bc529578cbcc5
visualstudio <visualstudio@122-PC16>
2019-01-30 10:56:37 -0500

    after change

---
+++ c0726863-WEEK-04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c0726863_WEEK_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
